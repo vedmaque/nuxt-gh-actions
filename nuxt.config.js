b0382c7873cb9679a76b3b300025f439f0cf8b90
Artyom Mikhaylov <vedmaque@gmail.com>
2021-03-14 14:09:15 +0300

    change router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/nuxt-gh-actions/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-gh-actions',
